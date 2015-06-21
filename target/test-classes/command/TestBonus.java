78176d6cd4b185b3f14de18090abf686c9b67392
Jonathan Martins <jonhmbc@gmail.com>
2015-06-20 21:42:35 -0300

    Testando teste

---
+++ target/test-classes/command/TestBonus.java
@@ -0,0 +1,15 @@
+import junit.framework.TestCase;
+
+public class TestBonus extends TestCase{
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	protected void tearDown() throws Exception{
+		super.tearDown();
+	}
+	
+	public void testExecute(){
+		assertTrue(true);
+	}
+}
