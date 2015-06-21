3ead80d97e4c2ccacc7835227124eb534c1cde7f
Jonathan Martins <jonhmbc@gmail.com>
2015-06-21 17:27:02 -0300

    Criando as outras classes de Teste

---
+++ src/test/resources/model/TestAccount.java
@@ -0,0 +1,15 @@
+import junit.framework.TestCase;
+
+public class TestAccount extends TestCase{
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	protected void tearDown() throws Exception{
+		super.tearDown();
+	}
+	
+	public void testGetBalance(){
+		assertTrue(true);
+	}
+}
